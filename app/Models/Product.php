b60d8ef4f9da2bb4ffb3c2cd97185323753a64b0
Amanda Rozi Kurnia <amandarozikurnia@gmail.com>
2022-04-07 23:29:56 +0700

    feat: update scope popular query

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Product extends Model
 {
@@ -175,20 +176,24 @@ class Product extends Model
 	 */
 	public function scopePopular($query, $limit = 10)
 	{
-		$month = now()->format('m');
+		// $month = now()->format('m');
 
-		return $query->selectRaw('products.*, COUNT(order_items.id) as total_sold')
-			->join('order_items', 'order_items.product_id', '=', 'products.id')
-			->join('orders', 'order_items.order_id', '=', 'orders.id')
-			->whereRaw(
-				'orders.status = :order_satus AND MONTH(orders.order_date) = :month',
-				[
-					'order_status' => Order::COMPLETED,
-					'month' => $month
-				]
-			)
-			->groupBy('products.id')
-			->orderByRaw('total_sold DESC')
+		// return $query->selectRaw('products.*, COUNT(order_items.id) as total_sold')
+		// 	->join('order_items', 'order_items.product_id', '=', 'products.id')
+		// 	->join('orders', 'order_items.order_id', '=', 'orders.id')
+		// 	->whereRaw(
+		// 		'orders.status = :order_satus AND MONTH(orders.order_date) = :month',
+		// 		[
+		// 			'order_status' => Order::COMPLETED,
+		// 			'month' => $month
+		// 		]
+		// 	)
+		// 	->groupBy('products.id')
+		// 	->orderByRaw('total_sold DESC')
+		// 	->limit($limit);
+
+		return $query->selectRaw('products.*')
+			->orderBy('name')
 			->limit($limit);
 	}
 
